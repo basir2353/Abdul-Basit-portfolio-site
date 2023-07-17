0e60eeb47113c7436fd8fc7b07da46043aa8e20f
Abdulbasit335 <113461803+basir2353@users.noreply.github.com>
2023-07-17 09:30:52 -0700

    ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { IconButton } from "@material-ui/core";
-
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
 import GitHubIcon from "@material-ui/icons/GitHub";
@@ -11,6 +10,13 @@ function Home() {
   return (
     <div className="home">
       <div className="about">
+        <div className="picture-container">
+          <img
+            className="profile-picture"
+            src={`${process.env.PUBLIC_URL}/basit.jpg`}
+            alt="Profile"
+          />
+        </div>
         <h2>Hi, My Name is Abdul Basit</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
@@ -63,13 +69,12 @@ function Home() {
           <li className="item">
             <h2>Back-End</h2>
             <span>
-              NodeJS, .NET, ExpressJS, GraphQL, 
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+              NodeJS, .NET, ExpressJS, GraphQL, MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
             </span>
           </li>
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Java, Python,React,TypeScript, Go</span>
+            <span>JavaScript, Java, Python, React, TypeScript, Go</span>
           </li>
         </ol>
       </div>
