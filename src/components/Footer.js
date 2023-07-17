26ecf271a7cd982b9f375b765f99dc026a148403
Abdulbasit335 <113461803+basir2353@users.noreply.github.com>
2023-07-17 05:38:31 -0700

    yes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,39 +10,38 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-      <IconButton
-              color="primary"
-              component="a"
-              href="https://www.instagram.com/mabdulbasitdogar/"
-              target="_blank"
-            >
-              <InstagramIcon />
-            </IconButton>
         <IconButton
-              color="primary"
-              component="a"
-              href="https://github.com/basir2353"
-              target="_blank"
-            >
-              <GitHubIcon />
-            </IconButton>
+          color="primary"
+          component="a"
+          href="https://www.instagram.com/mabdulbasitdogar/"
+          target="_blank"
+        >
+          <InstagramIcon />
+        </IconButton>
         <IconButton
-              color="primary"
-              component="a"
-              href="https://www.facebook.com/mabdulbasit.dogar.1"
-              target="_blank"
-            >
-        <FacebookIcon />
-             
-            </IconButton>
+          color="primary"
+          component="a"
+          href="https://github.com/basir2353"
+          target="_blank"
+        >
+          <GitHubIcon />
+        </IconButton>
         <IconButton
-              color="primary"
-              component="a"
-              href="https://www.linkedin.com/in/abdul-basit-1a56b3275"
-              target="_blank"
-            >
-              <LinkedInIcon />
-            </IconButton>
+          color="primary"
+          component="a"
+          href="https://www.facebook.com/mabdulbasit.dogar.1"
+          target="_blank"
+        >
+          <FacebookIcon />
+        </IconButton>
+        <IconButton
+          color="primary"
+          component="a"
+          href="https://www.linkedin.com/in/abdul-basit-1a56b3275"
+          target="_blank"
+        >
+          <LinkedInIcon />
+        </IconButton>
       </div>
       <p> &copy; 2023 abdulbasit.com</p>
     </div>
