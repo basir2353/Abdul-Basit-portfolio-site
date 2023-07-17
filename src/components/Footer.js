bb023b70fa4039355b66d6d28b44b1390ff868a4
Abdulbasit335 <113461803+basir2353@users.noreply.github.com>
2023-07-17 05:28:25 -0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { IconButton } from "@material-ui/core";
 import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import "../styles/Footer.css";
@@ -9,10 +10,39 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
+      <IconButton
+              color="primary"
+              component="a"
+              href="https://www.instagram.com/mabdulbasitdogar/"
+              target="_blank"
+            >
+              <InstagramIcon />
+            </IconButton>
+        <IconButton
+              color="primary"
+              component="a"
+              href="https://github.com/basir2353"
+              target="_blank"
+            >
+              <GitHubIcon />
+            </IconButton>
+        <IconButton
+              color="primary"
+              component="a"
+              href="https://www.facebook.com/mabdulbasit.dogar.1"
+              target="_blank"
+            >
         <FacebookIcon />
-        <LinkedInIcon />
+             
+            </IconButton>
+        <IconButton
+              color="primary"
+              component="a"
+              href="https://www.linkedin.com/in/abdul-basit-1a56b3275"
+              target="_blank"
+            >
+              <LinkedInIcon />
+            </IconButton>
       </div>
       <p> &copy; 2023 abdulbasit.com</p>
     </div>
